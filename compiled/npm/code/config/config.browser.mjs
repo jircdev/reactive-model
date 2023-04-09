85b04d535a931d20c35ef16f6abb8062873ff3b0
Julio Rodriguez <jircdev@gmail.com>
2023-04-09 18:11:15 -0300

    Feat: Add naming

--- compiled/npm/code/config/config.browser.mjs
+++
@@ -1,1 +0,0 @@
-export default {"package":"@beyond-js/reactive","version":"0.0.3","languages":{"default":"en","supported":["en","es"]},"global.css":true,"layout":"main-layout","params":{},"ssr":{},"backend":{}};
