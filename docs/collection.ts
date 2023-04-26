826c2ffb0c2ef8443fc4edaf16a6d3b63292b873
Julio Rodriguez <jircdev@gmail.com>
2023-04-26 09:23:47 -0300

    Feat: Add docs

---
+++ docs/collection.ts
@@ -0,0 +1,28 @@
+La clase `Collection` extiende de `ReactiveModel` y representa una colección de objetos reactivos. Está diseñada para manejar la carga, almacenamiento y sincronización de un conjunto de elementos, tanto en una base de datos local como en un servidor remoto.
+
+`Collection` utiliza una serie de clases y métodos auxiliares, como `CollectionLocalProvider`, `CollectionSaveManager`, `CollectionLoadManager` y una interfaz `ICollectionProvider`. Estas clases y métodos manejan diferentes aspectos de las operaciones de carga, almacenamiento y sincronización de datos.
+
+La clase `CollectionLocalProvider` se encarga de interactuar con la base de datos local, usando `Dexie` para las operaciones con IndexedDB. La clase `CollectionSaveManager` maneja el guardado de los datos tanto local como en el servidor remoto, mientras que `CollectionLoadManager` se encarga de cargar los datos desde la base de datos local y el servidor remoto.
+
+Aquí tienes un resumen de cómo funcionan las clases y métodos auxiliares:
+
+1. `CollectionLocalProvider`: Interactúa con la base de datos local, maneja eventos de conexión y guarda datos localmente.
+2. `CollectionSaveManager`: Guarda datos localmente usando `CollectionLocalProvider`, publica datos en el servidor remoto y sincroniza datos no sincronizados.
+3. `CollectionLoadManager`: Carga datos desde la base de datos local y el servidor remoto, y procesa las entradas devueltas.
+
+Un ejemplo de cómo se podría utilizar la clase `Collection` sería creando una clase `UserCollection` que extienda de `Collection`:
+
+```typescript
+import { Collection } from "@beyond-js/reactive/entities";
+import { User } from "./user";
+
+class UserCollection extends Collection {
+  constructor() {
+    super();
+    this.item = User;
+    this.init();
+  }
+}
+```
+
+En este ejemplo, se crea la clase `UserCollection` que hereda de `Collection`. Se especifica que los elementos de esta colección serán objetos de la clase `User`. Para utilizar esta clase, simplemente se instanciaría un objeto `UserCollection` y se utilizarían los métodos heredados como `load`, `save`, `sync`, etc., para interactuar con la base de datos local y remota.
---
+++ docs/item.ts
@@ -0,0 +1,45 @@
+
+## Item
+
+La clase Item es una clase abstracta que extiende de ReactiveModel. Esta clase representa un objeto "reactivo" en el sentido de que puede manejar cambios en sus propiedades y actualizar automáticamente otras partes del código, gracias a la herencia de ReactiveModel.
+
+Item tiene un proveedor local (LocalProvider) y un proveedor remoto (provider). El proveedor local maneja las operaciones de carga y almacenamiento en la base de datos local (como IndexedDB) mientras que el proveedor remoto interactúa con un servidor externo.
+
+La clase Item también incluye instancias de ItemSaveManager y ItemLoadManager para manejar las operaciones de guardado y carga, respectivamente. Estos administradores trabajan en conjunto con los proveedores locales y remotos para sincronizar los datos.
+
+El siguiente ejemplo de uso crea una clase User que extiende de Item:
+
+```
+import { ReactiveModel } from "@beyond-js/reactive/model";
+import { Item } from "@beyond-js/reactive/entities";
+import { UserProvider } from "@beyond-js/reactive/tests/backend/provider";
+
+interface IUser {
+	name?: string;
+	password: string;
+	lastnames: string;
+}
+
+export /*bundle */ class User extends Item<IUser> {
+	protected properties = ["id", "name", "lastname"];
+
+	protected storeName = "user";
+	protected db = "test";
+
+	constructor({ id = undefined } = {}) {
+		super(id);
+		this.provider = new UserProvider();
+		this.init({ id });
+	}
+
+	example() {
+		//    this.name = "algo";
+	}
+}
+
+```
+
+
+En este ejemplo, se define la clase User que hereda de Item. La clase User tiene propiedades como id, name y lastname, y utiliza un proveedor de usuario UserProvider para interactuar con un servidor externo. También define el nombre de la tienda local (storeName) y el nombre de la base de datos local (db).
+
+Para usar esta clase, simplemente instancie un objeto User y utilice sus métodos heredados como save, load, sync, etc., para interactuar con la base de datos local y remota.
