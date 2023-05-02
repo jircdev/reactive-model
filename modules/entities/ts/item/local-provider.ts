65750db578235af82b9ba9a3a486cd5e6d6fd1b1
ftovar8 <felix.godo@gmail.com>
2023-05-02 16:13:05 -0400

    feat: item - found property

--- modules/entities/ts/item/local-provider.ts
+++ modules/entities/ts/item/local-provider.ts
@@ -7,175 +7,178 @@ import { FactoryRecords } from "../registry/factory";
 import type { Registry } from "../registry";
 export /*bundle*/
 class LocalProvider extends ReactiveModel<IProvider> {
-	#isOnline = globalThis.navigator.onLine;
-	#store!: Dexie.Table<any, any>;
-	get store() {
-		return this.#store;
-	}
+  #isOnline = globalThis.navigator.onLine;
+  #store!: Dexie.Table<any, any>;
+  get store() {
+    return this.#store;
+  }
 
-	#offline: boolean;
-	#database!: DatabaseManager;
-	#storeName!: string;
-	#databaseName!: string;
-	#originalData: {};
-	#exists = false;
-	#found = false;
+  #offline: boolean;
+  #database!: DatabaseManager;
+  #storeName!: string;
+  #databaseName!: string;
+  #originalData: {};
+  #exists = false;
+  #found = false;
 
-	get originalData() {
-		return this.#originalData;
-	}
+  get originalData() {
+    return this.#originalData;
+  }
 
-	#db: Dexie;
+  #db: Dexie;
 
-	get isOnline() {
-		return this.#isOnline && !this.#offline && !localStorage.getItem("reactive.offline");
-	}
+  get isOnline() {
+    return this.#isOnline && !this.#offline && !localStorage.getItem("reactive.offline");
+  }
 
-	#parent;
-	#getProperty;
-	/**
-	 * @type {FactoryRecords}
-	 */
-	#records: FactoryRecords;
-	/**
-	 * @type {Registry} Database Record
-	 *
-	 */
-	#registry;
+  #parent;
+  #getProperty;
+  /**
+   * @type {FactoryRecords}
+   */
+  #records: FactoryRecords;
+  /**
+   * @type {Registry} Database Record
+   *
+   */
+  #registry;
 
-	get registry() {
-		return this.#registry;
-	}
-	constructor(parent, getProperty) {
-		super();
-		this.#getProperty = getProperty;
-		const { db, storeName } = parent;
-		this.__id = Math.floor(Math.random() * (100000 - 1000 + 1)) + 1000;
-		this.#parent = parent;
+  get registry() {
+    return this.#registry;
+  }
+  constructor(parent, getProperty) {
+    super();
 
-		if (!db || !storeName) throw new Error("database and store are required");
-		this.#databaseName = db;
-		this.#storeName = storeName;
-		this.#records = FactoryRecords.get(db);
-		globalThis.addEventListener("online", this.handleConnection);
-		globalThis.addEventListener("offline", this.handleConnection);
-	}
+    this.reactiveProps<IColleciton>(["found"]);
+    this.#getProperty = getProperty;
+    const { db, storeName } = parent;
+    this.__id = Math.floor(Math.random() * (100000 - 1000 + 1)) + 1000;
+    this.#parent = parent;
 
-	setOffline(value) {
-		this.#offline = value;
-		this.triggerEvent();
-	}
+    if (!db || !storeName) throw new Error("database and store are required");
+    this.#databaseName = db;
+    this.#storeName = storeName;
+    this.#records = FactoryRecords.get(db);
+    globalThis.addEventListener("online", this.handleConnection);
+    globalThis.addEventListener("offline", this.handleConnection);
+  }
 
-	init = async (id: string | number) => {
-		try {
-			const database: DatabaseManager = await DBManager.get(this.#databaseName);
-			this.#database = database;
-			this.#store = database.db[this.#storeName];
+  setOffline(value) {
+    this.#offline = value;
+    this.triggerEvent();
+  }
 
-			return this.load({ id });
-		} catch (e) {
-			console.error(e);
-		}
-	};
+  init = async (id: string | number) => {
+    try {
+      const database: DatabaseManager = await DBManager.get(this.#databaseName);
+      this.#database = database;
+      this.#store = database.db[this.#storeName];
 
-	private handleConnection = () => {
-		this.triggerEvent();
-		console.log("cambio la conexión");
-	};
+      return this.load({ id });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-	isUnpublished(data) {
-		const properties = Object.keys(data);
-		if (!this.#originalData) return true;
-		return properties.some(prop => this.#originalData[prop] !== data[prop]);
-	}
+  private handleConnection = () => {
+    this.triggerEvent();
+    console.log("cambio la conexión");
+  };
 
-	async load({ id = undefined } = {}) {
-		id = id ?? this.#parent.id;
+  isUnpublished(data) {
+    const properties = Object.keys(data);
+    if (!this.#originalData) return true;
+    return properties.some(prop => this.#originalData[prop] !== data[prop]);
+  }
 
-		try {
-			if (!id) throw new Error("id is required");
-			await this.#getRegistry(id);
-			this.#parent.loaded = true;
-			this.#parent.set(this.#registry.values);
-		} catch (e) {
-			console.error(e);
-		}
-	}
+  async load({ id = undefined } = {}) {
+    id = id ?? this.#parent.id;
 
-	/**
-	 * Retrieves the record from the local database store
-	 *
-	 * Also creates a listener for the record changes
-	 * @param id id of the record
-	 * @returns
-	 */
-	#getRegistry = async id => {
-		const registry = await this.#records.load(this.#storeName, id);
+    try {
+      if (!id) throw new Error("id is required");
+      await this.#getRegistry(id);
+      this.#parent.loaded = true;
+      this.#parent.found = true;
+      this.#parent.set(this.#registry.values);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
-		this.#parent.set(registry.values);
-		this.#registry = registry;
+  /**
+   * Retrieves the record from the local database store
+   *
+   * Also creates a listener for the record changes
+   * @param id id of the record
+   * @returns
+   */
+  #getRegistry = async id => {
+    const registry = await this.#records.load(this.#storeName, id);
 
-		registry.on("change", this.#listenRegistry);
-		return registry;
-	};
+    this.#parent.set(registry.values);
+    this.#registry = registry;
 
-	/**
-	 * Trigger the event to update the component when the registry changes.
-	 */
-	#listenRegistry = async () => {
-		if (!this.#registry) return;
-		this.#parent.set(this.#registry.values);
-		this.trigger("change");
-	};
+    registry.on("change", this.#listenRegistry);
+    return registry;
+  };
 
-	async save(data, backend = false) {
-		try {
-			if (!this.isUnpublished) return;
-			if (!this.isOnline) data.offline = true;
-			if (!this.#registry) {
-				await this.#getRegistry("new");
-			}
+  /**
+   * Trigger the event to update the component when the registry changes.
+   */
+  #listenRegistry = async () => {
+    if (!this.#registry) return;
+    this.#parent.set(this.#registry.values);
+    this.trigger("change");
+  };
 
-			// Add validation for unique fields
-			const duplicated = await this.validateUniqueFields(data);
-			if (duplicated.length) {
-				return { error: "duplicated", fields: duplicated };
-			}
-			await this.#registry.update(data, backend);
-			this.#parent.set(this.#registry.values);
+  async save(data, backend = false) {
+    try {
+      if (!this.isUnpublished) return;
+      if (!this.isOnline) data.offline = true;
+      if (!this.#registry) {
+        await this.#getRegistry("new");
+      }
 
-			return this;
-		} catch (e) {
-			console.log(20, "error saving", e);
-		}
-	}
+      // Add validation for unique fields
+      const duplicated = await this.validateUniqueFields(data);
+      if (duplicated.length) {
+        return { error: "duplicated", fields: duplicated };
+      }
+      await this.#registry.update(data, backend);
+      this.#parent.set(this.#registry.values);
 
-	async #update(data) {
-		try {
-			if (!this.isUnpublished) return;
-			await this.#store.update(data.id, data);
-			// recordsFactory.update(this.#storeName, data.id, data);
-		} catch (e) {}
-	}
+      return this;
+    } catch (e) {
+      console.log(20, "error saving", e);
+    }
+  }
 
-	async validateUniqueFields(data) {
-		if (!this.#getProperty("unique").length) return [];
+  async #update(data) {
+    try {
+      if (!this.isUnpublished) return;
+      await this.#store.update(data.id, data);
+      // recordsFactory.update(this.#storeName, data.id, data);
+    } catch (e) {}
+  }
 
-		const checkPromises = this.#getProperty("unique").map(field =>
-			this.#store
-				.where(field)
-				.equals(data[field])
-				.count()
-				.then(count => {
-					if (count) {
-						return field;
-					}
-					return null;
-				})
-		);
+  async validateUniqueFields(data) {
+    if (!this.#getProperty("unique").length) return [];
 
-		const duplicateFields = (await Promise.all(checkPromises)).filter(field => field !== null);
+    const checkPromises = this.#getProperty("unique").map(field =>
+      this.#store
+        .where(field)
+        .equals(data[field])
+        .count()
+        .then(count => {
+          if (count) {
+            return field;
+          }
+          return null;
+        })
+    );
 
-		return duplicateFields;
-	}
+    const duplicateFields = (await Promise.all(checkPromises)).filter(field => field !== null);
+
+    return duplicateFields;
+  }
 }
