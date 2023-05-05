4ef389ec3a6640dcb05515cb22f22e1a039facbe
Julio Rodriguez <jircdev@gmail.com>
2023-05-05 16:47:38 -0300

    fix: Make store available

--- modules/tests/models/ts/users.ts
+++ modules/tests/models/ts/users.ts
@@ -1,14 +1,14 @@
-import { Collection } from '@beyond-js/reactive-2/entities';
-import { User } from './user';
-import { UserProvider } from '@beyond-js/reactive-2/tests/backend/provider';
+import { Collection } from "@beyond-js/reactive-2/entities";
+import { User } from "./user";
+import { UserProvider } from "@beyond-js/reactive-2/tests/backend/provider";
 interface IUsers {
 	items: User[];
 }
 export /*bundle*/ class Users extends Collection {
 	item = User;
-	protected storeName = 'user';
-	protected db = 'test';
-	protected localdb = false;
+	protected storeName = "user";
+	protected db = "test";
+
 	constructor() {
 		super();
 		this.provider = new UserProvider();
