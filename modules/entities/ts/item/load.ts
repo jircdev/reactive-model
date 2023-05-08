a7fc1fbf48ddb1c420cc727905b5cde0fd907568
Julio Rodriguez <jircdev@gmail.com>
2023-05-08 17:13:12 -0300

    feat: Remove consoles

--- modules/entities/ts/item/load.ts
+++ modules/entities/ts/item/load.ts
@@ -1,71 +1,71 @@
 import type { Item } from ".";
 export class ItemLoadManager {
-  #parent: Item<any>;
+	#parent: Item<any>;
 
-  #localProvider;
-  #provider;
-  #getProperty;
-  constructor(parent, getProperty) {
-    this.#parent = parent;
-    this.#getProperty = getProperty;
+	#localProvider;
+	#provider;
+	#getProperty;
+	constructor(parent, getProperty) {
+		this.#parent = parent;
+		this.#getProperty = getProperty;
 
-    this.init();
-  }
+		this.init();
+	}
 
-  init = async () => {
-    this.#parent.load = this.load;
+	init = async () => {
+		this.#parent.load = this.load;
 
-    this.#localProvider = this.#getProperty("localProvider");
-    this.#provider = this.#getProperty("provider");
-  };
+		this.#localProvider = this.#getProperty("localProvider");
+		this.#provider = this.#getProperty("provider");
+	};
 
-  /**
-   *
-   * @param id
-   * @returns
-   */
-  load = async (params: any) => {
-    try {
-      await this.#getProperty("checkReady")();
-      let id = params.id;
-      const parent = this.#parent;
-      parent.fetching = true;
-      if (!id) id = parent.id;
+	/**
+	 *
+	 * @param id
+	 * @returns
+	 */
+	load = async (params: any) => {
+		try {
+			await this.#getProperty("checkReady")();
+			let id = params.id;
+			const parent = this.#parent;
+			parent.fetching = true;
+			if (!id) id = parent.id;
 
-      if (await this.#getProperty("localdb")) {
-        const localData = await this.#localProvider.load(params);
-        if (localData) this.#parent.set(localData);
-      }
+			if (await this.#getProperty("localdb")) {
+				const localData = await this.#localProvider.load(params);
+				if (localData) this.#parent.set(localData);
+			}
 
-      if (this.#localProvider && !this.#localProvider.isOnline) return;
+			if (this.#localProvider && !this.#localProvider.isOnline) return;
 
-      if (!this.#provider) return console.warn("No provider");
+			if (!this.#provider) return console.warn("No provider");
 
-      const remoteData = await this.remoteLoad(params);
-      if (!remoteData) this.#parent.found = false;
+			const remoteData = await this.remoteLoad(params);
+			if (!remoteData) this.#parent.found = false;
 
-      if (remoteData) {
-        let same = true;
-        Object.keys(remoteData).forEach(key => {
-          let original = this.#localProvider.registry.values;
-          if (original[key] !== remoteData[key]) same = false;
-        });
+			if (remoteData) {
+				let same = true;
+				Object.keys(remoteData).forEach(key => {
+					let original = this.#localProvider.registry.values;
+					if (original[key] !== remoteData[key]) same = false;
+				});
 
-        if (!same) await this.#localProvider.save(remoteData);
-      }
-      return { status: true, data: remoteData };
-    } catch (exc) {
-      console.error("ERROR LOAD", exc.message);
-      return { status: false, error: exc };
-    } finally {
-      this.#parent.fetching = false;
-    }
-  };
+				if (!same) await this.#localProvider.save(remoteData);
+			}
+			return { status: true };
+		} catch (exc) {
+			console.error("ERROR LOAD", exc.message);
+			return { status: false, error: exc };
+		} finally {
+			this.#parent.fetching = false;
+		}
+	};
 
-  remoteLoad = async params => {
-    // TODO: CHANGE TO LOAD
-    const response = await this.#provider.data(params);
-    if (!response.status) throw "ERROR_DATA_QUERY";
-    return response.data;
-  };
+	remoteLoad = async params => {
+		// TODO: CHANGE TO LOAD
+		const response = await this.#provider.data(params);
+		if (!response.status) throw "ERROR_DATA_QUERY";
+		return response.data;
+	};
 }
