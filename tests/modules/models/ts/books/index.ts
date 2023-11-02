0701ae2c59509dc5eca54006c858220f875416b4
Julio Rodriguez <jircdev@gmail.com>
2023-11-02 10:08:51 -0300

    chore: test code

--- tests/modules/models/ts/books/index.ts
+++ tests/modules/models/ts/books/index.ts
@@ -1,14 +1,23 @@
 import { Item, IItem } from '@beyond-js/reactive/entities';
 // import { UserProvider } from '@beyond-js/reactive/tests/backend/provider';
+import { Author } from 'author';
 interface IBook extends IItem {
 	name?: string;
 	password: string;
 	lastnames: string;
 }
 
+export interface IProvider {}
 export /*bundle */
-class Book extends Item<IBook> {
-	protected properties = ['id', 'title', 'author', 'year'];
+class Book extends Item<IBook, IProvider> {
+	protected properties = [
+		'id',
+		'title',
+		{
+			type: Author,
+		},
+		'year',
+	];
 
 	constructor({ id = undefined } = {}) {
 		super({ id, storeName: 'book', db: 'test' });
