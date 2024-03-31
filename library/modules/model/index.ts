7ac07e3d1d66c58d375a2b507006c6379578f0b9
Julio Rodríguez <jircdev@gmail.com>
2024-03-31 08:28:46 -0300

    fix: Solve types

--- library/modules/model/index.ts
+++ library/modules/model/index.ts
@@ -3,87 +3,81 @@ import { reactiveProps } from './property';
 import { IReactiveProperties } from './interfaces/reactive-props';
 import { ReactiveModelPublic } from './interfaces/reactive-public-props';
 import { IReactiveConstructorSpecs } from './interfaces/reactive-constructor-specs';
-import { IInitialValues } from './interfaces/initial-values';
 
 /**
- * The ReactiveModel class extends Events to provide reactive properties and event triggering
- * when properties change. It includes methods to set and get properties while ensuring type safety.
+ * The `ReactiveModel` class is a subclass of the `Events` class that provides a simple way to create
+ * reactive properties that can trigger events when they change. It also provides methods for setting
+ * and getting property values.
  *
- * @template T The type of properties defined in the model.
+ * @template T - The type of the properties that can be defined in the model.
  * @extends Events
  */
-export abstract class ReactiveModel<T> extends Events {
-	#isReactive: boolean = true;
 
-	/**
-	 * Indicates if the model is reactive.
-	 */
-	get isReactive(): boolean {
+export /*bundle*/ abstract class ReactiveModel<T> extends Events {
+	protected schema: unknown;
+	#isReactive: boolean = true;
+	get isReactive() {
 		return this.#isReactive;
 	}
+	[key: string]: any;
+	fetching!: boolean;
+	fetched: boolean = false;
+	processing: boolean = false;
+	ready: boolean = false;
+	processed: boolean = false;
+	protected properties: string[];
+	loaded: boolean = false;
 
-	protected properties: (keyof T)[];
-	#initialValues: IInitialValues<T> = {};
-
-	/**
-	 * Determines if the model has unpublished changes.
-	 */
-	get isUnpublished(): boolean {
+	#initialValues: Record<string, any> = {};
+	get isUnpublished() {
 		const properties = this.getProperties();
-		return properties.some(prop => {
+		return Object.keys(properties).some(prop => {
 			if (prop === 'id' || typeof prop === 'object') return false;
 			return properties[prop] !== this.#initialValues[prop];
 		});
 	}
-
-	/**
-	 * Constructs a new ReactiveModel instance.
-	 *
-	 * @param specs The specifications including initial properties.
-	 */
-	constructor(specs: IInitialValues<T> = {}) {
+	constructor(specs: IReactiveConstructorSpecs = {}) {
 		super();
-		this.properties = Object.keys(specs) as (keyof T)[];
-		this.initialValues(specs);
+		this.reactiveProps<IReactiveProperties>(['fetching', 'fetched', 'processing', 'processed', 'loaded', 'ready']);
+
+		if (specs.properties && Array.isArray(specs.properties)) {
+			this.properties = specs.properties;
+		}
+		if (specs) this.initialValues(specs);
 	}
 
-	/**
-	 * Sets or retrieves initial property values.
-	 *
-	 * @param values The initial property values to set.
-	 * @returns The current initial values if no arguments are provided.
-	 */
-	initialValues(values?: IInitialValues<T>): void | IInitialValues<T> {
+	initialValues(values?) {
 		if (!values) return this.#initialValues;
 		let data = { ...values };
+		delete data.properties;
+
 		this.#set(data);
 		this.#initialValues = data;
 	}
 
-	/**
-	 * Defines reactive properties for the model.
-	 *
-	 * @param props An array of property names to make reactive.
-	 */
-	protected reactiveProps(props: (keyof T)[]): void {
-		props.forEach(prop => {
-			this.defineReactiveProp(prop, this.#initialValues[prop]);
-		});
+	protected reactiveProps<T>(props: Array<keyof T>): void {
+		for (const propKey of props) {
+			const descriptor = Object.getOwnPropertyDescriptor(this, propKey);
+			const initialValue = descriptor ? descriptor.value : undefined;
+
+			this.defineReactiveProp(propKey, initialValue);
+		}
 	}
 
-	/**
-	 * Defines a single reactive property.
-	 *
-	 * @param propKey The name of the property to make reactive.
-	 * @param initialValue The initial value of the property.
-	 */
-	protected defineReactiveProp(propKey: keyof T, initialValue: T[keyof T]): void {
+	protected defineReactiveProp<T>(propKey: keyof T, initialValue: T[keyof T]): void {
+		const privatePropKey = `__${String(propKey)}`;
+
 		Object.defineProperty(this, propKey, {
-			get: () => this.#initialValues[propKey],
-			set: (newVal: T[keyof T]) => {
-				if (newVal === this.#initialValues[propKey]) return;
-				this.#initialValues[propKey] = newVal;
-				this.triggerEvent('change');
+			get(): T[keyof T] {
+				if (!this.hasOwnProperty(privatePropKey)) {
+					this[privatePropKey] = initialValue;
+				}
+				return this[privatePropKey];
+			},
+			set(newVal: T[keyof T]): void {
+				if (newVal === this[privatePropKey]) return;
+				this[privatePropKey] = newVal;
+				this.triggerEvent();
 			},
 			enumerable: true,
 			configurable: true,
@@ -91,41 +85,77 @@ export abstract class ReactiveModel<T> extends Events {
 	}
 
 	/**
-	 * Triggers an event to notify of property changes.
+	 * The `triggerEvent` method triggers a change event on the model, which can be used to notify
+	 * subscribers of changes to the model's properties.
 	 *
-	 * @param event The name of the event to trigger.
+	 * @param {string} event - The name of the event to trigger.
+	 * @returns {void}
 	 */
-	protected triggerEvent(event: string = 'change'): void {
-		setTimeout(() => this.trigger(event), 0);
-	}
-
+	triggerEvent = (event: string = 'change'): void => {
+		globalThis.setTimeout(() => {
+			this.trigger(event);
+		}, 0);
+	};
 	/**
-	 * Sets one or more properties on the model.
+	 * The `set` method sets one or more properties on the model.
 	 *
-	 * @param properties An object with properties to set.
+	 * @param {keyof ReactiveModelPublic<T>} property - The name of the property to set.
+	 * @param {*} value - The value to set the property to.
+	 * @returns {void}
 	 */
-	set(properties: Partial<T>): void {
+	#set(properties: Partial<T>): void {
 		let updated = false;
-		Object.keys(properties).forEach(prop => {
-			const propKey = prop as keyof T;
-			const newVal = properties[propKey];
-			if (newVal !== undefined && newVal !== this[propKey]) {
-				this[propKey] = newVal;
+		try {
+			Object.keys(properties).forEach(prop => {
+				if (!this.properties || !this.properties.includes(prop)) return;
+				const sameObject =
+					typeof properties[prop] === 'object' &&
+					JSON.stringify(properties[prop]) === JSON.stringify(this[prop]);
+
+				if (this[prop] === properties[prop] || sameObject) return;
+				const descriptor = Object.getOwnPropertyDescriptor(this, prop);
+				if (descriptor?.set) return;
+
+				this[prop] = properties[prop];
 				updated = true;
-			}
-		});
-		if (updated) this.triggerEvent();
+			});
+		} catch (e) {
+			throw new Error(`Error setting properties: ${e}`);
+		} finally {
+			if (updated) this.triggerEvent();
+		}
 	}
 
-	/**
-	 * Retrieves the current property values of the model.
-	 *
-	 * @returns An object with the current values of the model's properties.
-	 */
-	getProperties(): IInitialValues<T> {
-		const props: IInitialValues<T> = {} as IInitialValues<T>;
-		this.properties.forEach(prop => {
-			props[prop] = this[prop];
+	set(properties: Partial<ReactiveModelPublic<T>>): void {
+		this.#set(properties);
+	}
+
+	getProperties(): Record<string, any> {
+		const props: Record<string, any> = {};
+		const properties = this.properties || this.skeleton;
+
+		type IProperty = {
+			name: string;
+			type: string;
+		};
+		properties.forEach((property: string | IProperty) => {
+			if (typeof property === 'object') {
+				if (!property.hasOwnProperty('name')) return;
+				type ICollection = {
+					type: string;
+					name: string;
+				};
+
+				const collection = property as ICollection;
+				if (collection.type === 'collection') {
+					props[property.name] = this[property.name].items.map((item: any) => item.getProperties());
+					return;
+				}
+				props[property.name] = this[property.name];
+			}
+			let name = property as string;
+
+			props[name] = this[name];
 		});
 		return props;
 	}
